443d37003429d2ed5166c66feebafe5acf86aa83
Harvey Shi <harveyyshi@gmail.com>
2018-04-18 15:19:07 -0400

    Combines main.py and endpoints.py. - merges login code with other endpoints, fixes #21 - adds validation for login - removes pytest cache, fixes #20 - adds action.py file template

---
+++ actions.py
@@ -0,0 +1,40 @@
+from flask import jsonify
+from model import User
+from pymodm.errors import DoesNotExist
+from pymodm import connect
+import logging
+
+connect("mongodb://localhost:27017/database")
+
+
+def act_login(req):
+    '''Authenticates login with email
+
+    : param req: json request from client
+    '''
+    email = req['email']
+
+    try:
+        user = User.objects.raw({'_id': email}).first()
+    except DoesNotExist:
+        logging.exception('User does not exist.')
+        res = {
+            'email': email,
+            'login_success': False,
+            'error': 'User does not exist'
+        }
+        return (jsonify(res), 400)
+
+    return jsonify({'email': email, 'login_success': True})
+
+
+def act_upload(req):
+    pass
+
+
+def act_process(req):
+    pass
+
+
+def act_download(req):
+    pass
--- model.py
+++ model.py
@@ -4,5 +4,5 @@ from pymodm import fields, MongoModel
 class User(MongoModel):
         email = fields.EmailField(primary_key=True)
         password = fields.CharField()
-        origImageLink = field.URLField()
-        currentImageLink = field.URLField()
+        origImageLink = fields.URLField()
+        currentImageLink = fields.URLField()
