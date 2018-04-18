9bac4d11a6d2f788266da135340c9b29e72bf4a7
Harvey Shi <harveyyshi@gmail.com>
2018-04-18 13:08:26 -0400

    Restructures endpoints slightly. - adds validation module

--- endpoints.py
+++ endpoints.py
@@ -1,49 +1,46 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from validate import val_upload, val_process, val_download
+from actions import action_upload, action_process, action_download
 import datetime
 
 app = Flask(__name__)
 CORS(app)
 
 
+def handler(input, validator, action):
+    """Handles API endpoints
+
+    :param input: input data from request
+    :param validator: validator function to use
+    :param action: database function to apply to data
+    :return: jsonified response
+    """
+
+    if(not validator(input)):
+        response = {
+            'message': '[ERROR] input was incorrectly formatted.',
+            'code': 400
+        }
+    else:
+        response = action(input)
+        code = 200
+    return response, code
+
+
 @app.route("/upload", methods=["POST"])
 def upload():
     r = request.get_json()
-    username = r["username"]
-    if not validate_whitelist_email(username):
-        res = {
-            "Message": "Username is not valid"
-        }
-        code = 400
-    elif not validate_image_format(r['file']):
-        res = {
-            "Message": "Image is not in the correct format. It must be a JPEG, PNG, or TIFF file"
-        }
-        code = 400
-    else:
-        file = r['file']
-        fileID = upload_to_server(username, file)
-        res = {
-            "Message": "Image {} successfully added to database".format(fileID)
-        }
-        code = 200
-    return jsonify(res), code
+    return handler(r, val_upload, action_upload)
 
 
 @app.route("/process", methods=["POST"])
 def process():
     r = request.get_json()
-    username = r["username"]
-    if not validate_whitelist_email(username):
-        res = {
-            "Message": "Username is not valid"
-        }
-        code = 400
-    else:
-        fileID = r['fileID']
-        processedfile = segment_image(username, fileID)
-        res = {
-            "Message": "Image successfully processed",
-        }
-        code = 200
-    return jsonify(res), code
+    return handler(r, val_process, action_process)
+
+
+@app.route("/download", methods=["POST"])
+def download():
+    r = request.get_json()
+    return handler(r, val_download, action_download)
