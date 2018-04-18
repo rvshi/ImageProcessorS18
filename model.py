78176424322b388049591cc5460f792601609011
Edward Liang <Edward@Edwards-MacBook-Pro.local>
2018-04-18 11:56:02 -0400

    Changed schema for segmentation (current, original image)

--- model.py
+++ model.py
@@ -4,4 +4,6 @@ from pymodm import fields, MongoModel
 class User(MongoModel):
         email = fields.EmailField(primary_key=True)
         password = fields.CharField()
-        currentImage = fields.ImageField()
+        origImageLink = field.URLField()
+        currentImageLink = field.URLField()
+
