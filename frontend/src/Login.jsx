9ae7be6c27f820e021b7f3de10e95519938e7fd5
Harvey Shi <harveyyshi@gmail.com>
2018-04-23 10:38:41 -0400

    Updates names.

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Login.css';
 
 const Login = ({ request, update, email, password }) => (
     <div className="Login">
         <div className="Login-circle">
-            <h1>Super Pixel </h1>
-            <h2>Welcome to the future of image segmentation.</h2>
+            <h1>HYPERPIXEL</h1>
+            <h2>Welcome to the future of image segmentation technology.</h2>
 
             <input type="text" placeholder="username" autoComplete="off" autoFocus value={email} onChange={e => update('email', e.target.value)} />
             <input type="password" placeholder="password" autoComplete="off" value={password} onChange={e => update('password', e.target.value)} />
