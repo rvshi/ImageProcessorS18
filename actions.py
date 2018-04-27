6563dae0c34b8c9272ea3dc1b3750d39d99b7ac5
Michelle Wei <michellewei12345@gmail.com>
2018-04-27 13:15:42 -0400

    v1 of segmentation code

--- actions.py
+++ actions.py
@@ -3,15 +3,19 @@ from model import User
 from pymodm.errors import DoesNotExist
 from pymodm import connect
 import logging
+from segment import segment
+import scipy.io
+import matplotlib
+import model
 
 connect("mongodb://localhost:27017/database")
 
 
 def act_login(req):
-    '''Authenticates login with email
+    """Authenticates login with email
 
-    : param req: json request from client
-    '''
+        :param req: json request from client
+    """
     email = req['email']
 
     try:
@@ -29,11 +33,14 @@ def act_login(req):
 
 
 def act_upload(req):
+    """Uploads user image and processes image
+
+        :param req: .mat file from client
+    """
+    matfile = req['matfile']
+    segment(matfile)
     pass
 
-
-def act_process(req):
-    pass
 
 
 def act_download(req):
