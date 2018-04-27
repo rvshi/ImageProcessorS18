75c7b2f6cc1517db53c14570f0b5f131bd91cb31
Harvey Shi <harveyyshi@gmail.com>
2018-04-27 17:16:18 -0400

    Updates to image upload.

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,7 +4,8 @@ import Login from './Login';
 import Dashboard from './Dashboard';
 import Notifications from './Notifications';
 
-
+const MAX_FILE_SIZE = 1024 * 1024 * 4;
+const SUPPORTED_FILE_TYPES = ['image/jpeg', 'image/png'];
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,7 +13,8 @@ class App extends Component {
       username: '',
       password: '',
       jwt: null,
-      loggedIn: false
+      loggedIn: false,
+      images: null
     };
   }
 
@@ -53,6 +55,35 @@ class App extends Component {
     localStorage.setItem('jwt', 'undefined'); // remove jwt
   }
 
+  upload = () => {
+    this.refs.imageUploader.click();
+  }
+
+  handleFile = (files) => {
+
+    // validate file
+    if (files.length === 1) {
+      let file = files[0];
+      if (file.size > MAX_FILE_SIZE) {
+        this.notify('File is larger than 4MB', 'bad');
+        return;
+      } else if (!SUPPORTED_FILE_TYPES.includes(file.type)) {
+        this.notify('File must be JPEG or PNG', 'bad');
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.setState({
+          images: {
+            original: reader.result
+          }
+        });
+      };
+    }
+  }
+
   request = (path) => {
     const { username, password } = this.state;
     if (path === 'login') {
@@ -70,14 +101,16 @@ class App extends Component {
   }
 
   render() {
-    const { username, password, loggedIn, notification } = this.state;
+    const { username, password, loggedIn, notification, images } = this.state;
     return (
       <div className="App">
         {loggedIn ?
-          <Dashboard update={this.update} request={this.request} logout={this.logout} username={username} />
+          <Dashboard update={this.update} request={this.request} logout={this.logout} username={username} upload={this.upload} images={images} />
           : <Login update={this.update} request={this.request} username={username} password={password} />
         }
         {notification && <Notifications notification={notification} />}
+
+        <input type="file" id="file" ref="imageUploader" onChange={(e) => this.handleFile(e.target.files)} style={{ display: "none" }} />
       </div>
     );
   }
--- frontend/src/components/Dashboard/index.jsx
+++ frontend/src/components/Dashboard/index.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import Menubar from './Menubar';
 import './Dashboard.css';
 
-const Dashboard = ({ request, update, logout, username, password }) => (
-  <div className="Dashboard">
-    <Menubar logout={logout} username={username} />
+const Dashboard = ({ request, update, logout, username, upload, images }) => {
+  images && console.log(images['original']);
 
-    <div className="ImageViewer">
-      <div>Image</div>
+  return (
+    <div className="Dashboard">
+      <Menubar logout={logout} username={username} />
 
-      <button>Load image</button>
-      <button>Process image</button>
-      <button>Download image as JPEG</button>
+      <div className="Viewer">
+        {images && <img src={images.original} alt="Original" />}
+
+        <button onClick={upload}>Upload image</button>
+        <button>Reprocess image</button>
+        <button>Download image as JPEG</button>
+      </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default Dashboard;
