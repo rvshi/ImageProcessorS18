4613c1b9410bb40d958146f594492ee4a4567b16
Michelle Wei <michellewei12345@gmail.com>
2018-04-28 20:32:42 -1000

    clear actions.py for restructuring

--- actions.py
+++ actions.py
@@ -1,47 +1,50 @@
 from flask import jsonify
-from model import User
+from flask import jsonify, request
+from flask_jwt_simple import create_jwt
+
 from pymodm.errors import DoesNotExist
 from pymodm import connect
-import logging
+
 from segment import segment
-import scipy.io
-import matplotlib
-import model
+from database import login_user, save_image, get_image
 
 connect("mongodb://localhost:27017/database")
 
 
 def act_login(req):
     """Authenticates login with email
-
         :param req: json request from client
     """
-    email = req['email']
+    params = req.get_json()
+    username = params.get('username', None)
+    password = params.get('password', None)
 
-    try:
-        user = User.objects.raw({'_id': email}).first()
-    except DoesNotExist:
-        logging.exception('User does not exist.')
-        res = {
-            'email': email,
-            'login_success': False,
-            'error': 'User does not exist'
-        }
-        return (jsonify(res), 400)
+    # check if user exists and the password is correct
+    if login_user(username, password):
+        res = {'jwt': create_jwt(identity=username)}
+        return jsonify(res), 200
 
-    return jsonify({'email': email, 'login_success': True})
+    return jsonify({'error': 'username or password is incorrect'}), 400
 
 
 def act_upload(req):
-    """Uploads user image and processes image
-
-        :param req: .mat file from client
+    """Uploads user image, processes image, and downloads the processed image
+        :param req: json request from client
     """
-    matfile = req['matfile']
-    segment(matfile)
     pass
 
+
+def act_process(req):
+    """Processes image that has already been uploaded
+        :param req: json request from client
+    """
+    pass
 
 
 def act_download(req):
-    pass
+    """Processes download request
+        :param req: json request from client
+    """
+    img_str = get_image(req.username)
+    filetype = req['filetype']i
+    return jsonify({'image': img_str, 'filetype': filetype})
