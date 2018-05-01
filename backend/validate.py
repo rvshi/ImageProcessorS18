c555b825de8d9ae9283d56ae4ed9f94178bc0b54
Michelle Wei <michellewei12345@gmail.com>
2018-05-01 11:15:21 -1000

    add validate docs

--- backend/validate.py
+++ backend/validate.py
@@ -56,6 +56,12 @@ download_format = {
 
 
 def val_wrapper(input, schema_format):
+    """Validation endpoints
+
+        :param input: input json data from request
+        :param schema_format: schema format to use
+        :returns: validation true or false
+    """
     try:
         validate(input, schema_format)
         return True
@@ -64,16 +70,36 @@ def val_wrapper(input, schema_format):
 
 
 def val_login(input):
+    """Validate login format for username and password
+
+        :param input: input json data from user request
+        :return: validation true or false
+    """
     return val_wrapper(input, login_format)
 
 
 def val_upload(input):
+    """Validate upload format for username and b64 image file
+
+        :param input: input json data from user request
+        :return: validation true or false
+    """
     return val_wrapper(input, upload_format)
 
 
 def val_process(input):
+    """Validate process format for username
+
+        :param input: input json data from user request
+        :return: validation true or false
+    """
     return val_wrapper(input, process_format)
 
 
 def val_download(input):
+    """Validate download format for username and image uuid
+
+        :param input: input json data from user request
+        :return: validation true or false
+    """
     return val_wrapper(input, download_format)
